a6686b9b95eb0c1abe92d173c6da7e388e987ffe
masv2 <ahmadma@visa.com>
2017-03-01 17:26:44 +0000

    added Hack exercise

---
+++ hack_ex/Hack1/Hack1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack1
+{
+    class Program
+    {
+
+
+        /* 
+         * Staircase input a integer and outputs a staircase    
+         * 
+         * x
+         * xx
+         * xxx
+         * xxxx
+         * xxxxx
+         * xxxxxx
+         * 
+         * 
+         * Summation input a array and get  total content sum
+         *  e.g.  1,2,3,4 = 10
+         * 
+         * */
+
+
+        static void Main(string[] args)
+        {
+
+            StairCase(6);
+            Console.ReadLine();
+
+
+            int[] arr1 = new int[] { 1,2,3,4 };
+            
+           Console.WriteLine ( summation(arr1));
+            Console.ReadLine();
+        }
+
+
+        static void StairCase(int n)
+        {
+            for (int i = 1; i < n+1; i++)
+            {
+               string x= string.Concat(Enumerable.Repeat("*", i));
+               Console.WriteLine(x);
+            }
+
+        }
+
+
+
+        static int summation(int[] numbers)
+        {
+
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            
+            return sum;
+
+        }
+
+
+
+
+    }
+}
---
+++ hack_ex/Hack2/Hack2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack2
+{
+    class Program
+    {
+       
+
+
+            static void Main(string[] args)
+	{
+		Circle C1 = new Circle( Convert.ToDouble(Console.ReadLine()) );
+		Console.WriteLine( C1.getArea() );
+
+        double[] dimensions = Console.ReadLine().Split().Select(x => Convert.ToDouble(x)).ToArray();
+        Rectangle R1 = new Rectangle(dimensions[0], dimensions[1]);
+        Console.WriteLine(R1.getArea());
+
+        Circle C2 = new Circle(Convert.ToDouble(Console.ReadLine()));
+        Console.WriteLine(C2.getArea());
+
+        Square S1 = new Square(Convert.ToDouble(Console.ReadLine()));
+        Console.WriteLine(S1.getArea());
+
+        dimensions = Console.ReadLine().Split().Select(x => Convert.ToDouble(x)).ToArray();
+        Rectangle R2 = new Rectangle(dimensions[0], dimensions[1]);
+        Console.WriteLine(R2.getArea());
+
+        Console.ReadLine();
+
+        }
+    }
+
+
+
+
+   
+
+
+   
+
+
+
+    public class Circle 
+    {
+
+        double r;
+        public Circle( double radius)
+        {
+            r = radius;
+           
+        }
+
+        public double getArea()
+        {
+            return  Math.Ceiling(3.14159265 * r * r);
+        }
+
+    }
+
+
+
+    public class Rectangle 
+    {
+
+        double h, w;
+        public Rectangle(double height, double width)
+        {
+            w = width;
+            h = height;
+        }
+
+        public double getArea()
+        {
+            return Math.Ceiling(w * h);
+        }
+
+    }
+
+
+    public class Square 
+    {
+
+        double w;
+        public Square(double width)
+        {
+            w = width;
+        }
+
+        public double getArea()
+        {
+            return Math.Ceiling(w *w);
+        }
+
+    }
+
+}

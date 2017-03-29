6f810e0ae5f8d0076ae7869b3835903bf5fcd212
Mas <masahmad7@gmail.com>
2017-03-29 22:07:53 +0100

    playlist module update

--- requirejs/amd/lib/modules/playlist.js
+++ requirejs/amd/lib/modules/playlist.js
@@ -44,16 +44,74 @@ var jsonobj;
 	
     });
   }
-	  	  
 	
+
+	
+	// processes the template part 
 	,processPlaylist(json) {
 		alert('ok am processing' + json.length);
 		
 		  $(json).each(function(i,item){
         alert(i + ' ' + item.playlistname.name + '   timeout:' + item.playlistname.templatetimeout);
+		//
+		Playlist.processWidget(item.playlistname.playlists);
     });
 		
 	}
+  
+  // processes each widget
+,processWidget(widgets) {
+	
+	alert('w');
+	 console.log(widgets);
+	
+	 $(widgets).each(function(i,w) {
+		 alert(i + ' ' + w.templateinstance.templateId +'  ' +  w.templateinstance.idtemplateInstance );
+		 
+		 var templateid = w.templateinstance.templateId;
+		 var templateinstanceid = w.templateinstance.idtemplateinstance;
+		 
+		  switch(templateid) {
+    case 1:
+        alert('1');
+        break;
+    case 2:
+        alert('2');
+        break;
+	
+case 3:
+        alert('3');
+        break;
+		
+		case 4:
+        alert('4');
+        break;
+	
+	case 5:
+        alert('5');
+        break;
+	
+	case 6:
+        alert('6');
+        break;
+	
+	
+    default:
+       alert('xxx');
+} 
+		 
+		 
+	 });
+	 
+	 
+	 
+	 
+	 
+	
+	 
+	 
+}
+  
   
   ,getPrivateFN: function() { return privateVar;}
   
